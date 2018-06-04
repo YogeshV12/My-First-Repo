2231b9a9f5975581968ff018aa461c216b185b06
YogeshV12 <yogesh.verma.chy16@itbhu.ac.in>
2018-06-04 15:08:14 +0530

    First Commit

---
+++ h.py
@@ -0,0 +1,29 @@
+class yogi:
+
+	def __init__(self1, data2):
+		self1.data3 = data2
+		self1.next = None
+
+class LinkedList:
+
+	def __init__(self1):
+		self1.head = None
+
+	def printL(self1):
+		temp = self1.head
+		while(temp):
+			print (temp.data3)
+			temp = temp.next
+
+if __name__=='__main__':
+
+	list2 = LinkedList()
+
+	list2.head = yogi(1)
+	sec = yogi(2)
+	thrd = yogi(3)
+
+	list2.head.next = sec;
+	sec.next = thrd;
+
+	list2.printL()
